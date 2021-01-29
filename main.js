2b8896e19ca48b191febc3e062239ef552ddb061
JanHolecekProjects <jan.holecek@post.cz>
2021-01-29 12:55:55 +0100

    minor changes to bg generator

--- main.js
+++ main.js
@@ -5,55 +5,32 @@ let theDeleteButton = document.getElementById("delete-b");
 
 // USER INPUTS
 let userInputBlColor = document.getElementById("select-color");
-
-
-
-//POOLS
-//background pool
-let bg = [
-	//"grey",
-	"transparent",
-	//"black",
-	// "white",
-	// "steelBlue", 
-	// "red", 
-	// "blue", 
-	// "orange", 
-	// "lime",
-	// "white",
-	// "pink",
-	// "yellow",
-	// "linear-gradient(to right, blue, steelBlue)",
-	// "linear-gradient(to left, orange, yellow)",
-	// "linear-gradient(to right, red, orange);"
-];
-let bgCount = bg.length;
-
-// BLOCK POOL
-let bl = [
-	"transparent"	
-];
+let userInputBgColor = document.getElementById("select-bg");
 
 
 
 
 // GENERATE BUTTON
 theGenerateButton.addEventListener("click", generateBlocks);
+theGenerateButton.addEventListener("click", generateBackground);
 
 
-// background generator
-function ranNumBg() {
-	return Math.floor(Math.random() * bgCount);
+// BACKGROUND GENERATOR
+function generateBackground(e) {
+	e.preventDefault();
+
+	bg = [userInputBgColor.value];
+	insertBg(bg);
 };
 
-theGenerateButton.addEventListener("click", (e) => {
-	e.preventDefault();
-	frame.style.background = bg[ranNumBg()];
-	console.log(ranNumBg());
-	console.log("number of backgrounds in array = " + bgCount);
+//functions for background generator
+function insertBg(bg) {
+	let bgCount = bg.length;
+	frame.style.background = bg[ranNum(bgCount)];
 	frame.style.border = "0";
-});
-
+	console.log(ranNum(bgCount));
+	console.log("number of backgrounds in array = " + bgCount);
+};
 
 
 // BLOCK GENERATOR
@@ -90,7 +67,8 @@ theDeleteButton.addEventListener("click", (e) => {
 	e.preventDefault();
 	
 	frame.innerHTML = "";
-	frame.style.border = "1px solid black"
+	frame.style.border = "1px solid black";
+	frame.style.background = "";
 })
 
 
