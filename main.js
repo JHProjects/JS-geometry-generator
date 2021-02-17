89a9233fb1b9fae7c06e1d71894b610d8c6ee4c0
JanHolecekProjects <jan.holecek@post.cz>
2021-02-17 11:19:07 +0100

    added features
    block number input & multiple color inputs

--- main.js
+++ main.js
@@ -2,22 +2,54 @@ let frame = document.getElementById("frame");
 let block = document.getElementById("block");
 let theGenerateButton = document.getElementById("generate-b");
 let theDeleteButton = document.getElementById("delete-b");
+let plusOptionButtons = document.querySelector(".add-option-button");
 
 // USER INPUTS
+let userInputNumber = document.querySelector("#select-number");
+let colorSelection = document.querySelector("#color-selection");
+let userInputBlShape = document.getElementById("shape-selection");
 let userInputBlColor = document.getElementById("select-color");
 let userInputBgColor = document.getElementById("select-bg");
 
+// ADD MORE SELECTIONS
+plusOptionButtons.addEventListener("click", createExtraOption);
 
+//functions for clicking "extra option" button
+function createExtraOption(e) {
+	e.preventDefault();
+
+	colorSelection.insertAdjacentHTML("beforeend", `
+		<select>
+			${userInputBlColor.innerHTML}
+		</select>
+		`);
+
+};
 
 
 // GENERATE BUTTON
-theGenerateButton.addEventListener("click", generateBlocks);
-theGenerateButton.addEventListener("click", generateBackground);
+theGenerateButton.addEventListener("click", execute);
+theGenerateButton.addEventListener('keydown', executeShrt);
+
+function executeShrt(e) {
+	e.preventDefault();
+
+	if (e.key == "g") {
+		generateBlocks();
+		generateBackground();
+	}
+}
+
+function execute(e) {
+	e.preventDefault();
+
+	generateBlocks();	
+	generateBackground();
+};
 
 
 // BACKGROUND GENERATOR
-function generateBackground(e) {
-	e.preventDefault();
+function generateBackground() {
 
 	bg = [userInputBgColor.value];
 	insertBg(bg);
@@ -35,12 +67,18 @@ function insertBg(bg) {
 
 // BLOCK GENERATOR
 function generateBlocks(e) {
-	e.preventDefault();
 
 	bl = [
-		userInputBlColor.value,
+		colorSelection.children[0].value,
 		"transparent"
 	];
+
+	if (colorSelection.children[1]) {
+		bl.push(colorSelection.children[1].value)	
+	}
+	if (colorSelection.children[2]) {
+		bl.push(colorSelection.children[2].value)	
+	}
 	insertBlocks(bl);
 };
 
@@ -49,7 +87,18 @@ function insertBlocks(bl) {
 	let blCount = bl.length;
 
 	frame.innerHTML = "";
-	let numOfBlocks = 1024;
+
+	let oneSideCount = parseInt(userInputNumber.value);
+
+	if (oneSideCount <= 0 && oneSideCount >= 600) {
+		oneSideCount = 2;
+		alert("Choose number above '0' and below 600")
+	};
+	
+	let numOfBlocks = oneSideCount * oneSideCount;
+	frame.style.gridTemplateColumns = `repeat(${oneSideCount}, 1fr)`;
+	frame.style.gridTemplateRows = `repeat(${oneSideCount}, 1fr)`;
+	
 	let count = 1;
 	while (count <= numOfBlocks) {
 		frame.insertAdjacentHTML("beforeend", `
